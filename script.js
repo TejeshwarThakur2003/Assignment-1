6ffa4556b295f04f17701c0db52f3c9a723afcf6
Tejeshwar Thakur <tejeshwarthakur2003@gmail.com>
2024-10-08 12:49:29 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('nav ul');
+
+    if (menuToggle && navMenu) {
+        menuToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+        });
+    }
+});
